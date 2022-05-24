955039bdd890c22ce1038fae9856cc6f7f1be3fa
Patrick <patrick.astermann01@gmail.com>
2022-05-25 08:59:27 +1000

    Fixed up some sounds made a build for Itch.IO

--- SoundDesignProject/Assets/Scripts/PauseMenu.cs
+++ SoundDesignProject/Assets/Scripts/PauseMenu.cs
@@ -22,8 +22,8 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(pKey))
         {
-            aS.clip = sM.pauseMenuSfx;
-            aS.Play();
+            //aS.clip = sM.pauseMenuSfx;
+            //aS.Play();
             if (gameIsPaused)
             {
                 Resume();
